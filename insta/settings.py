92a3061b1f4240aee2f35a1e1716af70aec6dc02
dohyeongkim97 <redsun731118@naver.com>
2023-08-30 16:25:48 +0900

    posts, templates, requirements

--- insta/settings.py
+++ insta/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'posts',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
